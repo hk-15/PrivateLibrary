b70a295f6b3adb037fc7fd3985e06a15e0d46b63
Holly Knox <hollyknox15@gmail.com>
2025-07-21 11:14:46 +0100

    New migration

---
+++ api/Migrations/20250721101406_UpdateBookModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalLibraryBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBookModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EditionPublicationYear",
+                table: "Books");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EditionPublicationYear",
+                table: "Books",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
