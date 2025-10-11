c95415e311e3fb4b8b353d063b08019be4ba958d
Holly Knox <hollyknox15@gmail.com>
2025-10-11 08:45:36 +0100

    Sort collections on collections management page

--- ui/src/components/Collections/CollectionsManagement/CollectionsManagement.tsx
+++ ui/src/components/Collections/CollectionsManagement/CollectionsManagement.tsx
@@ -9,17 +9,22 @@ type Props = {
 export const CollectionsManagement: React.FC<Props> = ({ collections }) => {
     const [books, setBooks] = useState<Book[]>([]);
     const [fetchBooks, setFetchBooks] = useState(false);
+    const [sortedCollections, setSortedCollections] = useState<Collection[]>([]);
 
     useEffect(() => {
         getBooks("", "", "", "")
             .then(response => setBooks(response))
             .catch(err => console.error(err))
             setFetchBooks(false);
-    }, [collections, fetchBooks]);
-    
+    }, [fetchBooks]);
+
+    useEffect(() => {
+        setSortedCollections(collections.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)));
+    }, [collections])
+
     return (
         <div className="border-spaced-bottom">
-            {collections.sort().map(collection => {
+            {sortedCollections.map(collection => {
                 return <Collapsible key={collection.id} open={false} header={collection.name} books={books.filter(b => b.collection === collection.name)} collections={collections} getRefresh={setFetchBooks}/>
             })}
         </div>
