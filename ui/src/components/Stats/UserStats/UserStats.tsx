5fdaf7f8d61461ff584a6283121bfe566767ffad
Holly Knox <hollyknox15@gmail.com>
2026-01-20 17:01:13 +0000

    Hide user stats when no books have been added, run prettier on component

--- ui/src/components/Stats/UserStats/UserStats.tsx
+++ ui/src/components/Stats/UserStats/UserStats.tsx
@@ -4,47 +4,70 @@ import { UserBarCharts } from "../UserBarCharts/UserBarCharts";
 import { LoginContext } from "../../LoginManager/LoginManager";
 
 type Props = {
-    username: string;
-}
+  username: string;
+};
 
 export const UserStats: React.FC<Props> = ({ username }) => {
-    const loginContext = useContext(LoginContext);
-    const [books, setBooks] = useState<Book[]>([]);
+  const loginContext = useContext(LoginContext);
+  const [books, setBooks] = useState<Book[]>([]);
 
-    useEffect(() => {
-        getAllBooks()
-            .then(response => {
-                setBooks(response.filter(b => b.owner === loginContext.username))
-            })
-            .catch(err => console.error(err));
-    }, []);
+  useEffect(() => {
+    getAllBooks()
+      .then((response) => {
+        setBooks(response.filter((b) => b.owner === loginContext.username));
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
-    const numOfBooks = books.length;
-    const readPercentage = Math.round(books.filter(b => b.read).length / numOfBooks * 100);
-    const translatedPercentage = Math.round(books.filter(b => b.originalLanguage !== "" && b.originalLanguage !== null).length / numOfBooks * 100);
+  const numOfBooks = books.length;
+  const readPercentage = Math.round(
+    (books.filter((b) => b.read).length / numOfBooks) * 100,
+  );
+  const translatedPercentage = Math.round(
+    (books.filter(
+      (b) => b.originalLanguage !== "" && b.originalLanguage !== null,
+    ).length /
+      numOfBooks) *
+      100,
+  );
 
-    const authorFrequency: Record<string, number> = {};
-    books.flatMap(book => book.authors).forEach(author => {
-        authorFrequency[author] = (authorFrequency[author] || 0) + 1;
+  const authorFrequency: Record<string, number> = {};
+  books
+    .flatMap((book) => book.authors)
+    .forEach((author) => {
+      authorFrequency[author] = (authorFrequency[author] || 0) + 1;
     });
-    const authorData = Object.entries(authorFrequency)
-        .map(([author, count]) => ({ author, count, }))
-        .sort((a, b) => b.count - a.count);
+  const authorData = Object.entries(authorFrequency)
+    .map(([author, count]) => ({ author, count }))
+    .sort((a, b) => b.count - a.count);
 
-    return (
+  return (
+    <div>
+      {books.length > 0 && (
         <div className="border-spaced-bottom">
-            <h2>{username}&#8217;s stats </h2>
-            <div className="border-spaced-bottom">
-                <ul className="stats-highlights">
-                    <li><span className="stat-number">{numOfBooks}</span>books</li>
-                    <li><span className="stat-number">&nbsp;{readPercentage}%</span>read</li>
-                    <li><span className="stat-number">{authorData.length}</span>authors</li>
-                    <li><span className="stat-number">{translatedPercentage}%</span>translated</li>
-                </ul>
-            </div>
-            <div>
-                <UserBarCharts books={books} authorData={authorData} />
-            </div>
+          <h2>{username}&#8217;s stats </h2>
+          <div className="border-spaced-bottom">
+            <ul className="stats-highlights">
+              <li>
+                <span className="stat-number">{numOfBooks}</span>books
+              </li>
+              <li>
+                <span className="stat-number">&nbsp;{readPercentage}%</span>read
+              </li>
+              <li>
+                <span className="stat-number">{authorData.length}</span>authors
+              </li>
+              <li>
+                <span className="stat-number">{translatedPercentage}%</span>
+                translated
+              </li>
+            </ul>
+          </div>
+          <div>
+            <UserBarCharts books={books} authorData={authorData} />
+          </div>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
